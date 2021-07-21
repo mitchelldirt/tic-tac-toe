34efbb1e09d37ca0755f759f928d0b9c5414c20f
Mitchell Mudd <mitchellmudd@outlook.com>
2021-07-21 19:03:13 -0400

    Trying to add logic for adding game pieces (x's and o's)

--- scripts.js
+++ scripts.js
@@ -2,11 +2,27 @@
 const gameBoard = (() => {
     let gameBoardArray = [{ L1: "empty" }, { L2: "empty" }, { L3: "empty" }, { M1: "empty" }, { M2: "empty" },
     { M3: "empty" }, { R1: "empty" }, { R2: "empty" }, { R3: "empty" }];
+    const gameSquareDom = document.getElementsByClassName("gameSquare");
+
+    _addGamePiece = (function (gameSquareDom) {
+        for (let i = 0; i < gameSquareDom.length; i++) {
+            gameSquareDom.onClick = () => {
+                if (gameSquareDom[i] !== "empty") {
+                    break;
+                } else if (Player1 === "active") {
+                    gameSquareDom[i] = "x"
+                } else {
+                    gameSquareDom[i] = "o"
+                }
+            }
+        }
+    })
+
 })();
 
 // This module will control how the player can interact with `gameBoard`.
 const displayController = (() => {
-    
+
 })();
 
 // TODO: maybe add color as a property for Player in the future. Would change the color of the X and O
