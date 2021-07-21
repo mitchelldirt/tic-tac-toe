91fe61bd2a95a0609072eec44d6a30c635578f0e
Mitchell Mudd <mitchellmudd@outlook.com>
2021-07-21 13:14:47 -0400

    Started creating the logic of the game with a factory and two modules. Created tic tac toe grid display

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const gameBoard = (() => {
+    let gameBoardArray = [{ L1: "empty" }, { L2: "empty" }, { L3: "empty" }, { M1: "empty" }, { M2: "empty" },
+    { M3: "empty" }, { R1: "empty" }, { R2: "empty" }, { R3: "empty" }];
+})();
+
+const displayController = (() => {
+})();
+
+// maybe add color as a property for Player in the future. Would change the color of the X and O
+const Player = (name) => {
+    const getName = () => name;
+};
+
+
+
