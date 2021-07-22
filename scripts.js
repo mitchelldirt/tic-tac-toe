8b40d25f18fdd12314e749297d7793f87243306c
Mitchell Mudd <mitchellmudd@outlook.com>
2021-07-22 13:03:00 -0400

    Tried fixing logic. gameSquareDom is showing length of zero :(

--- scripts.js
+++ scripts.js
@@ -2,23 +2,41 @@
 const gameBoard = (() => {
     let gameBoardArray = [{ L1: "empty" }, { L2: "empty" }, { L3: "empty" }, { M1: "empty" }, { M2: "empty" },
     { M3: "empty" }, { R1: "empty" }, { R2: "empty" }, { R3: "empty" }];
-    const gameSquareDom = document.getElementsByClassName("gameSquare");
 
     //TODO: This function is probably wrong right now. Make sure you change the corresponding gameBoardArray item based on which square was clicked. Write this out
-    _addGamePiece = (function (gameSquareDom) {
-        for (let i = 0; i < gameSquareDom.length; i++) {
+    _findSquare = (function () {
+        const gameSquareDom = document.getElementsByTagName("button");
+        for (let i = 0; i < gameBoardArray.length; i++) {
+            console.log(gameSquareDom);
             gameSquareDom.onClick = () => {
-                if (gameSquareDom[i] !== "empty") {
-                    break;
-                } else if (Player1 === "active") {
-                    gameSquareDom[i] = "x"
-                } else {
-                    gameSquareDom[i] = "o"
+                // grab title of gamesquaredom
+                let location = gameSquareDom.title;
+                for (let i = 0; i < gameBoardArray.length; i++) {
+                    console.log(gameBoardArray[i])
+                    if (location === gameBoardArray[i]) {
+                        indexOfSquare = i;
+                        return;
+                    } else {
+                        console.log("Couldn't find location in array.");
+                        return;
+                    }
                 }
             }
         }
-    })
+    })()
+    // compare that title against gameboardarray
 
+    // return the index location of the gameboardarray value to a function that will check the value and change the value and add the game piece
+    /*
+    _changeSquare = (function (index) {
+        if (gameSquareDom[i].title !== "empty") {
+            return;
+        } else if (Player1 === "active") {
+            gameSquareDom[i] = "x";
+        } else {
+            gameSquareDom[i] = "o";
+        }
+    })() */
 })();
 
 // This module will control how the player can interact with `gameBoard`.
